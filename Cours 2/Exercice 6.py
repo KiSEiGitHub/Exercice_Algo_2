24456992312cfdb6c9404b6570c0c77fdcfbbeee
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-07 04:27:52 +0200

    Afficher chaine et liste

---
+++ Cours 2/Exercice 6.py
@@ -0,0 +1,14 @@
+def main():
+    chaine = input("Entrer une chaine: ")
+
+    for i in chaine:
+        print(i)
+
+    l = ['Chien', 'Chat', 'Vache', 'banane']
+
+    for i in l:
+        print(i)
+
+
+if __name__ == '__main__':
+    main()
