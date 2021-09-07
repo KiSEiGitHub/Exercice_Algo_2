99f29c90c4a956712572949c04d22f538c7e5d31
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-07 19:34:43 +0200

    m

--- Cours5/Exercice1/parfait_chanceux_m.py
+++ Cours5/Exercice1/parfait_chanceux_m.py
@@ -1,6 +1,14 @@
-def main():
-    pass
+def somDiv(x):
+    return sum(i for i in range(1, x + 1) if x % i == 0)
 
 
-if __name__ == '__main__':
-    main()
+def estParfait(x, y):
+    return x == y
+
+
+def estPremier(x):
+    return x == 1
+
+
+def estChanceux(x):
+    return all(estPremier(x + i + i ** 2) for i in range(x - 1))
