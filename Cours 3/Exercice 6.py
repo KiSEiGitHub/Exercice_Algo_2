e507b15bc067a9a8090cc72a896d59a7bad4f727
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-07 05:34:27 +0200

    Décompresse tuple

--- Cours 3/Exercice 6.py
+++ Cours 3/Exercice 6.py
@@ -1,6 +1,10 @@
-def main():
-    pass
+def somme(x):
+    return sum(x)
 
+
+def main():
+    t = [int(input("Entrer nombre: ")) for _ in range(3)]
+    print(sum(t))
 
 if __name__ == '__main__':
     main()
