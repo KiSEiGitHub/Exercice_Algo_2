a6dffba9588cf5463b656b10b85431997a0f9451
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-07 05:57:43 +0200

    Truc et machin

---
+++ Cours 4/Exercice 2.py
@@ -0,0 +1,17 @@
+def main():
+    truc = []
+    machin = [2.2, 2.7, 6.9, 5.2, 7.1]
+
+    print(f"Entier de 0 à 3: {range(4)}")
+    print(f"Entier de 4 à 7: {range(4, 8)}")
+    print("range(2, 9, 2) =", range(2, 9, 2))
+
+    chose = range(6)
+
+    print(f"Appartenance de 3 dans chose: {3 in chose}")
+    print(f"Appartenance de 6 dans chose: {6 in chose}")
+
+
+
+if __name__ == '__main__':
+    main()
