6b81456b90116dcc65cb06be304e20c5195c3d17
KiSEiGitHub <disfigurehollah@gmail.com>
2021-09-07 05:30:10 +0200

    Volume / Masse Elipsoïde

--- Cours 3/Exercice 4.py
+++ Cours 3/Exercice 4.py
@@ -1,13 +1,17 @@
 from math import pi
 
 
-def volume_masse_elip(a, b, c):
-    return 4 / 3 * pi * a * b * c
+def volume_masse_elip(a, b, c, x):
+    v = 4 / 3 * pi * a * b * c
+    m = v * x
+    return v, m
 
 
 def main():
-    volume = volume_masse_elip(10, 5, 2)
+    volume, masse = volume_masse_elip(10, 2, 5, 2)
     print(f"Volume = {round(volume, 2)}")
+    print(f"Masse = {round(masse, 2)}")
+
 
 
 if __name__ == '__main__':
